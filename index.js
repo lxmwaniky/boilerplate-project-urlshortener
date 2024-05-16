90d240e32f574fd5eda98439df998312081ca5b7
Alex Mwaniki <lekko254@gmail.com>
2024-05-16 08:25:25 +0000

    Break

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const { MongoClient } = require('mongodb');
 const client = new MongoClient(process.env.MONGODB_URL);
 const db = client.db('urlshortener');
 const urls = db.collection('urls');
+const urlparser = require('url');
+const dns = require('dns');
+const { url } = require('inspector');
+const { hostname } = require('os');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -22,8 +26,22 @@ app.get('/', function(req, res) {
 
 // Your first API endpoint
 app.post('/api/shorturl', function(req, res) {
-  console.log(req.body);
-  res.json({ greeting: 'hello API' });
+  const url = req.body.url;
+  const dnslookup = dns.lookup(urlparser.parse(url).hostname, async (err, address) => {
+    if (!address){
+      res.json({error: 'Invalid URL'});
+    } else {
+      const urlCount = await urls.countDocuments({});
+      const urlDoc = {
+        url,
+        short_url: urlCount
+      }
+
+      const result = await urls.insertOne(urlDoc);
+      console.log(result);
+      res.json({ original_url: url, short_url: urlCount });
+    }
+  })
 });
 
 app.listen(port, function() {
